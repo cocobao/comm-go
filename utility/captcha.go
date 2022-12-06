6f4ba5c61ee0d256db00ae864194abe3a28182c9
yebz <yebz@qhfct.com>
2022-12-06 09:40:44 +0800

    fix

--- utility/captcha.go
+++ utility/captcha.go
@@ -2,7 +2,7 @@ package utility
 
 import (
 	"github.com/afocus/captcha"
-	"github.com/mojocn/base64Captcha"
+	// "github.com/mojocn/base64Captcha"
 )
 
 var (
@@ -12,15 +12,15 @@ var (
 )
 
 //获取图片验证码
-func GetCaptcha(width, height, length int) (id string, b64s string, err error) {
-	driver := &base64Captcha.DriverDigit{
-		Height:   width,
-		Width:    height,
-		Length:   length,
-		MaxSkew:  0.1,
-		DotCount: 15,
-	}
+// func GetCaptcha(width, height, length int) (id string, b64s string, err error) {
+// 	driver := &base64Captcha.DriverDigit{
+// 		Height:   width,
+// 		Width:    height,
+// 		Length:   length,
+// 		MaxSkew:  0.1,
+// 		DotCount: 15,
+// 	}
 
-	c := base64Captcha.NewCaptcha(driver, capstore)
-	return c.Generate()
-}
+// 	c := base64Captcha.NewCaptcha(driver, capstore)
+// 	return c.Generate()
+// }
